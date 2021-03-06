897a4f8a0c2c5ae4522af3dcf1840155acfe2698
daniel <danielsoledad@gmail.com>
2021-03-06 02:27:14 -0800

    Adding target blank

--- src/compoments/FooterLinks/index.js
+++ src/compoments/FooterLinks/index.js
@@ -3,7 +3,7 @@ import Icon from "../icon/icon"
 const FooterLinks = ({href, icon, margin,t}) =>{
     return(
         <strong className={`footer-links ${margin}`}>
-            <a className="icon-color" href={href}>
+            <a className="icon-color" rel="noreferrer" target="_blank" href={href}>
                 <Icon icon={icon}/>
             </a>
         </strong>
