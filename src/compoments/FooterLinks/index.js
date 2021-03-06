29d720c6999e272041551f484b5330dd4330aa3a
daniel <danielsoledad@gmail.com>
2021-03-06 02:22:33 -0800

    adding href to footer

--- src/compoments/FooterLinks/index.js
+++ src/compoments/FooterLinks/index.js
@@ -3,7 +3,7 @@ import Icon from "../icon/icon"
 const FooterLinks = ({href, icon, margin,t}) =>{
     return(
         <strong className={`footer-links ${margin}`}>
-            <a className="icon-color" {...href}>
+            <a className="icon-color" href={href}>
                 <Icon icon={icon}/>
             </a>
         </strong>
