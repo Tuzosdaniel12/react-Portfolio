bfb2a06eceb48c35c043a56d6bd6ad6372673506
daniel <danielsoledad@gmail.com>
2021-03-06 13:42:55 -0800

    Adding period to resume

--- client/src/compoments/Card/compoments/CardFooter.js
+++ client/src/compoments/Card/compoments/CardFooter.js
@@ -4,7 +4,7 @@ const CardFooter = ({gitrepo, link}) =>{
         <footer className="card-footer has-text-centered pt-2">
             <small className="has-text-centered has-text-danger" style={{width: '100%'}}>
                 <a className="tag is-black has-text-danger is-size-7" rel="noreferrer" id="repo" href={gitrepo} target="_blank">Git Repo</a>
-                <span className= "has-text-white" > || </span>
+                <span className= "has-text-white" > | | </span>
                 <a className="tag is-black has-text-danger is-size-7" rel="noreferrer" id="demo" href={link} target="_blank">Live Demo</a>
             </small>
         </footer>
