90a82260783b22b418dfa8bd99ffa6cc2bb656d7
daniel <danielsoledad@gmail.com>
2021-03-06 13:31:35 -0800

    add or to links

--- client/src/compoments/Card/compoments/CardFooter.js
+++ client/src/compoments/Card/compoments/CardFooter.js
@@ -3,7 +3,8 @@ const CardFooter = ({gitrepo, link}) =>{
     return(
         <footer className="card-footer has-text-centered pt-2">
             <small className="has-text-centered has-text-danger" style={{width: '100%'}}>
-                <a className="tag is-black has-text-danger is-size-7" rel="noreferrer" id="repo" href={gitrepo} target="_blank">Git Repo</a>-
+                <a className="tag is-black has-text-danger is-size-7" rel="noreferrer" id="repo" href={gitrepo} target="_blank">Git Repo</a>
+                <span className= "has-text-white" > || </span>
                 <a className="tag is-black has-text-danger is-size-7" rel="noreferrer" id="demo" href={link} target="_blank">Live Demo</a>
             </small>
         </footer>
