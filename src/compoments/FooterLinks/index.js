d7d8114cf0f8d8372fd7ee24dd271e745662964d
daniel <danielsoledad@gmail.com>
2021-03-06 02:08:31 -0800

    adding resume

--- src/compoments/FooterLinks/index.js
+++ src/compoments/FooterLinks/index.js
@@ -1,6 +1,6 @@
 import Icon from "../icon/icon"
 
-const FooterLinks = ({href, icon, margin}) =>{
+const FooterLinks = ({href, icon, margin,t}) =>{
     return(
         <strong className={`footer-links ${margin}`}>
             <a className="icon-color" {...href}>
