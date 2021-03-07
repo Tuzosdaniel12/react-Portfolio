98a5006c519c0320196aff86b499eaa98e7da33e
daniel <danielsoledad@gmail.com>
2021-03-07 14:27:46 -0800

    adding env

--- client/src/compoments/Card/compoments/CardFooter.js
+++ client/src/compoments/Card/compoments/CardFooter.js
@@ -3,9 +3,9 @@ const CardFooter = ({gitrepo, link}) =>{
     return(
         <footer className="card-footer has-text-centered pt-2">
             <small className="has-text-centered has-text-danger" style={{width: '100%'}}>
-                <a className="tag is-black has-text-danger is-size-7" rel="noreferrer" id="repo" href={gitrepo} target="_blank">Git Repo</a>
+                <a className="button is-black is-small has-text-danger is-size-7" rel="noreferrer" id="repo" href={gitrepo} target="_blank">Git Repo</a>
                 <span className= "has-text-white" > | | </span>
-                <a className="tag is-black has-text-danger is-size-7" rel="noreferrer" id="demo" href={link} target="_blank">Live Demo</a>
+                <a className="button is-black is-small has-text-danger is-size-7" rel="noreferrer" id="demo" href={link} target="_blank">Live Demo</a>
             </small>
         </footer>
     )
