4965683a7d58ad215197c03a308e94e263086fec
daniel <danielsoledad@gmail.com>
2021-03-08 17:09:50 -0800

    server side compression

--- client/src/compoments/navItems/index.js
+++ client/src/compoments/navItems/index.js
@@ -8,9 +8,9 @@ function NavItems({toggleBurgerMenu}){
     return(
         <div id="navMenu" className="navbar-menu has-background-black">
             <div className="navbar-end custom-nav-menu">
-                <Link to="/" data-target="home" className="navbar-item navbar-mod" onClick={toggleBurgerMenu}>Home</Link>
-                <Link to="/About" data-target="about" className="navbar-item navbar-mod" onClick={toggleBurgerMenu}>About Me</Link>
-                <Link to="/Work" data-target="work-examples" className="navbar-item navbar-mod" onClick={toggleBurgerMenu}>Work</Link>
+                <a to="/" data-target="home" className="navbar-item navbar-mod" onClick={toggleBurgerMenu}>Home</a>
+                <a to="/About" data-target="about" className="navbar-item navbar-mod" onClick={toggleBurgerMenu}>About Me</a>
+                <a to="/Work" data-target="work-examples" className="navbar-item navbar-mod" onClick={toggleBurgerMenu}>Work</a>
                 <a href="./docs/DanielSoledadResume.pdf" className="navbar-item navbar-mod" onClick={toggleBurgerMenu} >Resume</a>
             </div>
         </div>
