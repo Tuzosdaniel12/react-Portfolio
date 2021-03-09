9263ed1b310cba518e410c510d7c2504f1c0f409
daniel <danielsoledad@gmail.com>
2021-03-08 17:15:08 -0800

    links to a

--- client/src/compoments/navItems/index.js
+++ client/src/compoments/navItems/index.js
@@ -8,9 +8,9 @@ function NavItems({toggleBurgerMenu}){
     return(
         <div id="navMenu" className="navbar-menu has-background-black">
             <div className="navbar-end custom-nav-menu">
-                <a to="/" data-target="home" className="navbar-item navbar-mod" onClick={toggleBurgerMenu}>Home</a>
-                <a to="/About" data-target="about" className="navbar-item navbar-mod" onClick={toggleBurgerMenu}>About Me</a>
-                <a to="/Work" data-target="work-examples" className="navbar-item navbar-mod" onClick={toggleBurgerMenu}>Work</a>
+                <a href="/" data-target="home" className="navbar-item navbar-mod" onClick={toggleBurgerMenu}>Home</a>
+                <a href="/About" data-target="about" className="navbar-item navbar-mod" onClick={toggleBurgerMenu}>About Me</a>
+                <a href="/Work" data-target="work-examples" className="navbar-item navbar-mod" onClick={toggleBurgerMenu}>Work</a>
                 <a href="./docs/DanielSoledadResume.pdf" className="navbar-item navbar-mod" onClick={toggleBurgerMenu} >Resume</a>
             </div>
         </div>
