f8ab4411b3d0b87ee3cbcd596eee96d98f9b4350
daniel <danielsoledad@gmail.com>
2021-03-31 22:32:53 -0700

    for got to remove if statement on server

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Serve up static assets (usually on heroku)
-//if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-////}
+}
 
 app.use(compression());
 // Define any API routes before this runs
