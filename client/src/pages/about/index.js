cd1e4b05cb62ac03330b58f8d6367684e11cd43f
daniel <danielsoledad@gmail.com>
2021-04-02 12:17:28 -0700

    adding new about me

--- client/src/pages/about/index.js
+++ client/src/pages/about/index.js
@@ -20,7 +20,7 @@ const About = () => {
                 <Column columnStyles="is-two-thirds-desktop has-text-white has-text-centered-mobile is-flex is-flex-direction-column is-justify-content-center">
                     <h1 className="is-size-4 ">About Me</h1>
                     <p className="is-size-5 pb-5">
-                         Hello, my name is Daniel. I’m a graduate of the University of Washington’s Fullstack Web Development certification program. I am a problem solver with the analytical skills necessary to create smooth, user-friendly applications. In my opinion, the challenge of exploring and learning new techniques is the most rewarding aspect of web development.; Please don't hesitate to reach out to me if you have any questions @ <span className="has-text-centered has-text-danger">danielsoledad@gmail.com, </span>or at any other contact methods below. I am always eager to grow in knowledge. Let's CODE!!!
+                         Hello, my name is Daniel, a Fullstack Web Developer. Focus on using analytical skills and problem solving skills to create user-friendly applications. The challenge of exploring and learning new techniques is the most rewarding aspect of web development and what love about this career.; Please don't hesitate to reach out to me if you have any questions @ <span className="has-text-centered has-text-danger">danielsoledad@gmail.com, </span>or at any other contact methods below. I am always eager to grow in knowledge. Let's CODE!!!
                     </p>
 
                     <div className="has-text-centered has-text-danger">
