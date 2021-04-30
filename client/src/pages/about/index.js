3418bd8ec8952655fc5b3ad9ef5c36f36bd993b4
daniel <danielsoledad@gmail.com>
2021-04-29 21:46:15 -0700

    adding new about me

--- client/src/pages/about/index.js
+++ client/src/pages/about/index.js
@@ -8,41 +8,41 @@ import "./style.sass"
 const About = () => {
      
     return (
-        <Section
-            cName={`pb-6 ${
-                window.location.pathname === "/About" ? "fadeIn" : "fadeOut"
-            }`}
-            id="aboutme">
-            <Columns columnsName="is-tablet about-me-div">
-                <Column columnStyles="">
-                    <div className="is-flex is-justify-content-center">
-                        <img className alt="Me" src="./images/me.png" />
-                    </div>
-                </Column>
+		<Section
+			cName={`pb-6 ${
+				window.location.pathname === "/About" ? "fadeIn" : "fadeOut"
+			}`}
+			id="aboutme">
+			<Columns columnsName="is-tablet about-me-div">
+				<Column columnStyles="">
+					<div className="is-flex is-justify-content-center">
+						<img className alt="Me" src="./images/me.png" />
+					</div>
+				</Column>
 
-                <Column columnStyles="is-two-thirds-desktop has-text-white has-text-centered-mobile is-flex is-flex-direction-column is-justify-content-center">
-                    <h1 className="is-size-4 ">About Me</h1>
-                    <p className="is-size-5 pb-5">
-                        Hello, my name is Daniel, Fullstack Developer, hard
-                        working and honest individual, always eager to learn new
-                        skills. Focus on using analytical and problem
-                        solving skills to create user-friendly applications. Please don't hesitate to reach
-                        out if you have any questions @{" "}
-                        <span className="has-text-centered has-text-danger">
-                            danielsoledad@gmail.com,{" "}
-                        </span>
-                        or at any other contact methods below. Let's CODE!!!
-                    </p>
+				<Column columnStyles="is-two-thirds-desktop has-text-white has-text-centered-mobile is-flex is-flex-direction-column is-justify-content-center">
+					<h1 className="is-size-4 has-text-danger">About Daniel</h1>
+					<p className="is-size-5 pb-5">
+						Hard working and honest Fullstack Developer, always
+						eager to learn new skills. Uses analytical and problem
+						solving skills to create user-friendly applications. If
+						you have any questions don't hesitate to reach out
+						at{" "}
+						<span className="has-text-centered has-text-danger">
+							danielsoledad@gmail.com,{" "}
+						</span>
+						or at any of the other contact methods listed below. Let's CODE!!!
+					</p>
 
-                    <div className="has-text-centered has-text-danger">
-                        {iconList.map(({ icon, text }) => (
-                            <Icon icon={icon} text={text} />
-                        ))}
-                    </div>
-                </Column>
-            </Columns>
-        </Section>
-    );
+					<div className="has-text-centered has-text-danger">
+						{iconList.map(({ icon, text }) => (
+							<Icon icon={icon} text={text} />
+						))}
+					</div>
+				</Column>
+			</Columns>
+		</Section>
+	);
 }
 
 export default About   
