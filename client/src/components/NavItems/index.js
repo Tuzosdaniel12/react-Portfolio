3feac76b7b3c999d571dd16b93417ef405e9bbde
daniel <danielsoledad@gmail.com>
2021-06-16 17:08:40 -0700

    nav bar

--- client/src/components/NavItems/index.js
+++ client/src/components/NavItems/index.js
@@ -11,7 +11,7 @@ function NavItems({toggleBurgerMenu}){
                 <Link to="/" data-target="home" className="navbar-item navbar-mod" onClick={toggleBurgerMenu}>Home</Link>
                 <Link to="/About" data-target="about" className="navbar-item navbar-mod" onClick={toggleBurgerMenu}>About Me</Link>
                 <Link to="/Work" data-target="work-examples" className="navbar-item navbar-mod" onClick={toggleBurgerMenu}>Work</Link>
-                <a href="./docs/DanielSoledadResume.pdf" className="navbar-item navbar-mod" onClick={toggleBurgerMenu} >Resume</a>
+                <a href="./docs/DanielSoledadResume.pdf" className="navbar-item navbar-mod" target="_blank" onClick={toggleBurgerMenu} >Resume</a>
             </div>
         </div>
     )
