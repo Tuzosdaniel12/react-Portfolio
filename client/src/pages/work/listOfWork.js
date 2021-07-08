4196a40d44f785f1955749ce286c9e2caf77bf64
daniel <danielsoledad@gmail.com>
2021-07-07 23:40:12 -0700

    new resume and updated work examples

--- client/src/pages/work/listOfWork.js
+++ client/src/pages/work/listOfWork.js
@@ -1,55 +1,55 @@
 const listOfWork = [
-  {
-    link: "https://dono-application.herokuapp.com/",
-    image: "./images/dono.png",
-    gitrepo: "https://github.com/abdulhakeemdahir/dono-application",
-    description:
-      "MERN Application, this app is a social media charity platform for Non-ProfitOrganizations and potential donors. We intend to create a Twitter-esque experience that will make it easier for more people to take part in funding charitable causes",
-    alt: "Dono Application Charity",
-  },
-  {
-    link: "https://workout-buddies.herokuapp.com/",
-    gitrepo: "https://github.com/Tuzosdaniel12/Workout-Buddies",
-    image: "./images/Untitled.png",
-    description:
-      "Sequelize, Node, Express, Handlebars application that allows the user to create workouts of their own that can be shared freely amongst fellow users of the application.",
-    alt: "Work Out Buddies",
-  },
-  {
-    link: "https://mfrisch87.github.io/MovieTrailerMatchUp/",
-    image: "./images/movie.png",
-    gitrepo: "https://github.com/mfrisch87/MovieTrailerMatchUp",
-    description:
-      "HTML, CSS, Javascript application that creates a hub of sorts where the user can access a variety of movies associated with their search query and have instant access to the most relevant movie trailer associated with each.",
-    alt: "Movie Trailer Match Up",
-  },
-  {
-    link: "https://google-books-search-and-s.herokuapp.com/",
-    image:
-      "https://github.com/Tuzosdaniel12/googleBooks/blob/main/client/public/images/page.png?raw=true",
-    gitrepo: "https://github.com/Tuzosdaniel12/googleBooks",
-    description:
-      "MERN application that fetches data to google api and uses react global state render, saved and save data to MongoDB",
-    alt: "Google Books",
-    secondRow: "second-row",
-  },
-  {
-    link: "https://eat-la-burger-please.herokuapp.com/",
-    image: "./images/burger-example.png",
-    gitrepo: "https://github.com/Tuzosdaniel12/Eat-Da-Burger",
-    description: "This application uses MySql custom ORM, the orm solves the ability to use crud dynamically",
-    alt: "Eat The Burger",
-    secondRow: "second-row",
-  },
-  {
-    link: "https://employee-direct-finder.herokuapp.com/",
-    image:
-      "https://github.com/Tuzosdaniel12/Emp-Directory/blob/main/images/empdirectory.png?raw=true",
-    gitrepo: "https://github.com/Tuzosdaniel12/Emp-Directory",
-    description: "React application that uses state to fetch data to an outside API and render data",
-    alt: "Employee Directory",
-    secondRow: "second-row",
-  },
+	{
+		link: "https://dono-application.herokuapp.com/",
+		image: "./images/dono.png",
+		gitrepo: "https://github.com/abdulhakeemdahir/dono-application",
+		description:
+			"MERN Application, this app is a social media charity platform for Non-ProfitOrganizations and potential donors. We intend to create a Twitter-esque experience that will make it easier for more people to take part in funding charitable causes",
+		alt: "Dono Application Charity"
+	},
+	{
+		link: "https://workout-buddies.herokuapp.com/",
+		gitrepo: "https://github.com/Tuzosdaniel12/Workout-Buddies",
+		image: "./images/Untitled.png",
+		description:
+			"Sequelize, Node, Express, Handlebars application that allows the user to create workouts of their own that can be shared freely amongst fellow users of the application.",
+		alt: "Work Out Buddies"
+	},
+	{
+		link: "https://mfrisch87.github.io/MovieTrailerMatchUp/",
+		image: "./images/movie.png",
+		gitrepo: "https://github.com/mfrisch87/MovieTrailerMatchUp",
+		description:
+			"HTML, CSS, Javascript application that creates a hub of sorts where the user can access a variety of movies associated with their search query and have instant access to the most relevant movie trailer associated with each.",
+		alt: "Movie Trailer Match Up"
+	},
+	{
+		link: "https://google-books-search-and-s.herokuapp.com/",
+		image: "https://github.com/Tuzosdaniel12/googleBooks/blob/main/client/public/images/page.png?raw=true",
+		gitrepo: "https://github.com/Tuzosdaniel12/googleBooks",
+		description:
+			"MERN application that fetches data to google api and uses react global state render, saved and save data to MongoDB",
+		alt: "Google Books",
+		secondRow: "second-row"
+	},
+	{
+		link: "https://whatsapp-simple-clone.herokuapp.com/ ",
+		image: "./images/whatsApp.png",
+		gitrepo: "https://github.com/Tuzosdaniel12/whats-app-clone",
+		description:
+			"WhatsApp clone, to challenge ability to match styling, and learn server-side rendering with Next.js",
+		alt: "WhatsApp",
+		secondRow: "second-row"
+	},
+	{
+		link: "feisbuc-clone.herokuapp.com/",
+		image: "./images/face.png",
+		gitrepo: "https://github.com/Tuzosdaniel12/facebook-clone",
+		description:
+			"WhatsApp clone, to challenge ability to match styling, and learn server-side rendering with Next.js.",
+		alt: "Facebook",
+		secondRow: "second-row"
+	}
 ];
 
 export default listOfWork
