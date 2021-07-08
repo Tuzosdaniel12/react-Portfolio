762c49cbe2f59601d827b9655b442a1a9dfb5110
daniel <danielsoledad@gmail.com>
2021-07-08 00:09:09 -0700

    buggy fix

--- client/src/pages/work/listOfWork.js
+++ client/src/pages/work/listOfWork.js
@@ -42,7 +42,7 @@ const listOfWork = [
 		secondRow: "second-row"
 	},
 	{
-		link: "https://feisbuc-clone.herokuapp.com/",
+		link: "https://feisbuc-clone.herokuapp.com",
 		image: "./images/face.png",
 		gitrepo: "https://github.com/Tuzosdaniel12/facebook-clone",
 		description:
